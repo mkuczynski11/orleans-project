139e88da4c80c3172e663d1424c47d25b1ef91d1
mkuczynski11 <59289239+mkuczynski11@users.noreply.github.com>
2023-12-29 18:20:46 +0100

    feat: added localhost silo config

--- OrleansProject/Program.cs
+++ OrleansProject/Program.cs
@@ -1,3 +1,5 @@
+using Orleans.Runtime;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,13 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Orleans configuration
+builder.Host.UseOrleans(siloBuilder =>
+{
+    siloBuilder.UseLocalhostClustering();
+    siloBuilder.AddMemoryGrainStorage("users");
+});
 
 var app = builder.Build();
 
